aaddc8f80de9171d7ae90d99c4442a8e744f39fc
Alecto <fomenkoandrey1978@gmail.com>
2024-07-10 20:51:02 +0300

    additional #2

--- 39-formik-bootstrap/src/components/RegistrationForm.tsx
+++ 39-formik-bootstrap/src/components/RegistrationForm.tsx
@@ -1,94 +1,44 @@
 import { Button } from 'react-bootstrap'
-import * as Yup from 'yup'
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-
-const validationSchema = Yup.object().shape({
-  username: Yup.string().required('Логін є обов’язковим полем'),
-  password: Yup.string().required('Пароль є обов’язковим полем'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password')], 'Паролі повинні співпасти')
-    .required('Підтвердження паролю є обов’язковим полем'),
-  email: Yup.string().email('Введіть коректний email').required('Email є обов’язковим полем')
-})
+import { Form, Formik } from 'formik'
+import InputField from './InputField.tsx'
+import { useRegistrationForm } from '../helpers/useRegistrationForm.ts'
 
 const RegistrationForm = () => {
-  // const [username, setUsername] = useState('')
-  // const [password, setPassword] = useState('')
-  // const [confirmPassword, setConfirmPassword] = useState('')
-  // const [email, setEmail] = useState('')
-  //
-  // const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   switch (e.target.name) {
-  //     case 'username':
-  //       setUsername(e.target.value)
-  //       break
-  //     case 'password':
-  //       setPassword(e.target.value)
-  //       break
-  //     case 'confirmPassword':
-  //       setConfirmPassword(e.target.value)
-  //       break
-  //     case 'email':
-  //       setEmail(e.target.value)
-  //       break
-  //     default:
-  //       break
-  //   }
-  // }
-  //
-  // const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault()
-  //   if (password !== confirmPassword) {
-  //     alert('Паролі не співпадають')
-  //     return
-  //   }
-  //   console.log({ username, password, email })
-  // }
-
+  const formikProps = useRegistrationForm()
   return (
-    <Formik
-      initialValues={{ username: '', password: '', confirmPassword: '', email: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values)
-        setSubmitting(true)
-      }}
-    >
-      {({ isSubmitting }) => (
+    <Formik {...formikProps}>
+      {({ isSubmitting, errors, touched, isValid }) => (
         <Form>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Логін
-            </label>
-            <Field type="text" placeholder="Введіть логін" className="form-control" name="username" />
-            <ErrorMessage name="username" component="div" className="text-danger" />
-          </div>
+          <InputField name="username" placeholder="Введіть логін" label="Логін" touched={touched} errors={errors} />
 
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Пароль
-            </label>
-            <Field type="password" placeholder="Введіть пароль" className="form-control" name="password" />
-            <ErrorMessage name="password" component="div" className="text-danger" />
-          </div>
+          <InputField
+            name="password"
+            type="password"
+            placeholder="Введіть пароль"
+            label="Пароль"
+            touched={touched}
+            errors={errors}
+          />
 
-          <div className="mb-3">
-            <label htmlFor="confirmPassword" className="form-label">
-              Підтвердження паролю
-            </label>
-            <Field type="password" placeholder="Підтвердьте пароль" className="form-control" name="confirmPassword" />
-            <ErrorMessage name="confirmPassword" component="div" className="text-danger" />
-          </div>
+          <InputField
+            name="confirmPassword"
+            type="password"
+            placeholder="Підтвердіть пароль"
+            label="Підтвердження паролю"
+            touched={touched}
+            errors={errors}
+          />
 
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <Field type="email" placeholder="Введіть email" className="form-control" name="email" />
-            <ErrorMessage name="email" component="div" className="text-danger" />
-          </div>
+          <InputField
+            name="email"
+            type="email"
+            placeholder="Введіть email"
+            label="Email"
+            touched={touched}
+            errors={errors}
+          />
 
-          <Button type="submit" variant="primary" disabled={isSubmitting}>
+          <Button type="submit" variant="primary" disabled={!isValid || isSubmitting}>
             Зареєструватися
           </Button>
         </Form>
